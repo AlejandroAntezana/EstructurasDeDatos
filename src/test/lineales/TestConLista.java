862a429212c4e1403ad1f9dc419b58ace6cbc951
Alejandro Antezana <alejandro.antezana@est.fi.uncoma.edu.ar>
2022-06-10 11:37:37 -0300

    una parte de la implementacion

---
+++ src/test/lineales/TestConLista.java
@@ -0,0 +1,15 @@
+package test.lineales;
+
+import lineales.dinamicas.Lista;
+
+public class TestConLista {
+    public static void main(String[] args) {
+        Lista l = new Lista();
+
+        System.out.println(l.recuperar(1));
+        System.out.println( "a".equals(l.recuperar(1)));
+
+
+    }
+
+}
